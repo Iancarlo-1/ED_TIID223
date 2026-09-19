1c97fec77600a87540b1c9b9d8dfc9f3d4a20510
Iancarlo-1 <125052983@upq.edu.mx>
2026-09-18 19:56:22 -0600

    Actividad en clase

---
+++ EstructuradeDatos/Arreglos.py
@@ -0,0 +1,41 @@
+#Declarando un arreglo
+numeros = [10,20,30,40,50]
+print (numeros[2])
+
+#Reasignar un valor 
+numeros[3]=15
+print(numeros)
+
+#Agregamos un valor al final del arreglo
+numeros.append(60)
+print(numeros)
+
+#Eliminar por pos.
+numeros.pop(1)
+print(numeros)
+
+#Eliminar por valor
+numeros.remove(30)
+print(numeros)
+
+
+frutas =["Mango","Manzana","Uva","Pera","Maracuya"]
+print(frutas)
+
+frutas.remove("Uva")
+print(frutas)
+
+frutas.pop(3)
+print(frutas)
+
+frutas.append("kiwi")
+print(frutas)
+
+frutas[2]="Fresa"
+print(frutas)
+
+arreglo=[]
+n = int(input("Ingresa el tamaño del arreglo: "))
+
+arreglo.append[0]= int(input("Ingresa el valor 0 "))
+
